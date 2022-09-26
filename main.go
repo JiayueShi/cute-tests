143fef8e0cdcaf63f3c0215fa65ed7580e97268e
JoyShi <joy.shi@sap.com>
2022-09-26 17:18:13 +0800

    Add router.

--- main.go
+++ main.go
@@ -3,16 +3,33 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
+
+// I'd like to use my own Router (^^)
+func NewRouter() http.Handler {
+	router := mux.NewRouter()
+	AttachRoutes(router)
+	return router
+}
+
+func AttachRoutes(router *mux.Router) {
+	router.HandleFunc("/hello", HelloHandler)
+}
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	// Stage 1: hello world
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "Hello World\n")
 	// Stage 2: get some data from redis
+
 	// Stage 3: get some data with filters
 }
 
 func main() {
-	http.HandleFunc("/", HelloHandler)
-	http.ListenAndServe(":8000", nil)
+	// I want to use router!
+	// http.HandleFunc("/", HelloHandler)
+	handler := NewRouter()
+	http.Handle("/", handler)
+	http.ListenAndServe(":8000", handler)
 }
